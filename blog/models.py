4a417511af0db89e6c6b2173db3039fc868728a4
Vitaly Klyhun <klygun90@gmail.com>
2021-08-13 17:34:25 +0300

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db.models.fields import CharField, DateField, TextField
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = CharField(max_length=200)
+    date = DateField()
+    description = TextField()
